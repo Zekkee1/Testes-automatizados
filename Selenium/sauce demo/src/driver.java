9f0c81a006362d605053b2c4f710ccf486a024a2
Zekkee1 <lmartins763@gmail.com>
2022-09-13 04:09:20 -0300

    commit

--- Selenium/sauce demo/src/driver.java
+++
@@ -1,8 +0,0 @@
-
-
-
-public class driver {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","\"C:\\Users\\lucas\\Documents\\chromedriver\\chromedriver\"");
-    }
-}
